05ea569ebc56e22c25749e8eec100a2b1527e318
Kubeyev Gamal <kubeyev.g@kolesa.kz>
2020-05-09 17:52:50 +0600

    Решение 1 задачи со стеками скобок

--- BasicDataStructures.playground/Contents.swift
+++ BasicDataStructures.playground/Contents.swift
@@ -1,102 +1,15 @@
 import UIKit
 
-/*
-Sample Input 1:
+let task1 = Task1()
+task1.isCorrectBrackets(brackets: "([](){([])})")
+task1.isCorrectBrackets(brackets: "()[]}")
+task1.isCorrectBrackets(brackets: "{{[()]]")
+task1.isCorrectBrackets(brackets: "foo(bar);")
+task1.isCorrectBrackets(brackets: "foo(bar[i);")
+task1.isCorrectBrackets(brackets: "(((())))))")
+task1.isCorrectBrackets(brackets: ")))))")
+task1.isCorrectBrackets(brackets: "123huod")
 
-([](){([])})
-Sample Output 1:
-
-Success
-Sample Input 2:
-
-()[]}
-Sample Output 2:
-
-5
-Sample Input 3:
-
-{{[()]]
-Sample Output 3:
-
-7
-*/
-
-/// - Parameter brackets: ( [ { ) ] }
-func isCorrectBrackets(brackets: String) {
-    func isClosedBracket(character: Character) -> Bool {
-        return [")", "}", "]"].contains(character)
-    }
-    
-    func isOppositeBracket(_ bracket: Character, for openBracket: Character) -> Bool {
-        return openBracket == "(" && bracket == ")" ||
-               openBracket == "[" && bracket == "]" ||
-               openBracket == "{" && bracket == "}"
-    }
-    
-    func isBracket(character: Character) -> Bool {
-        let brackets: [Character] = ["(", "[", "{", ")", "]", "}"]
-        return brackets.contains(character)
-    }
-    
-    let stack = Stack<(Character, Int)>()
-    var hasClosedBracketInString: Bool = false
-    for (index, character) in brackets.enumerated() {
-        if !isBracket(character: character) {
-            continue
-        }
-        
-        if isClosedBracket(character: character) {
-            hasClosedBracketInString = true
-            if let top = stack.top(), isOppositeBracket(character, for: top.0) {
-                stack.pop()
-            } else {
-                // перва
-                print("\(index+1)")
-                return
-            }
-        } else {
-            stack.add((character, index+1))
-        }
-    }
-    
-    if stack.isEmpty() {
-        if hasClosedBracketInString {
-            print("Success")
-        } else {
-            print("0")
-        }
-        
-        return
-    }
-    
-    if hasClosedBracketInString, let top = stack.top() {
-        print(top.1)
-        
-        return
-    }
-    
-    for (index, element) in brackets.enumerated() {
-        if !hasClosedBracketInString {
-            if isBracket(character: element) {
-                print(index+1)
-                return
-            }
-        }
-    }
-    
-    print("nothing")
-}
-// ((()))(
-// (((((
-isCorrectBrackets(brackets: "([](){([])})")
-isCorrectBrackets(brackets: "()[]}")
-isCorrectBrackets(brackets: "{{[()]]")
-isCorrectBrackets(brackets: "foo(bar);")
-isCorrectBrackets(brackets: "foo(bar[i);")
-isCorrectBrackets(brackets: "(((())))))")
-isCorrectBrackets(brackets: ")))))")
-isCorrectBrackets(brackets: "123huod")
-
-isCorrectBrackets(brackets: "(")
-isCorrectBrackets(brackets: "qwe(qwe")
-isCorrectBrackets(brackets: "()((([]))")
+task1.isCorrectBrackets(brackets: "(")
+task1.isCorrectBrackets(brackets: "qwe(qwe")
+task1.isCorrectBrackets(brackets: "()((([]))")
--- BasicDataStructures.playground/Sources/Task1/Task1.swift
+++ BasicDataStructures.playground/Sources/Task1/Task1.swift
@@ -1,2 +1,100 @@
 import Foundation
 
+/// https://stepik.org/lesson/41234/step/1?unit=19818
+///
+/// Задача со скобками
+/// Input: Любая строка с любыми char'ами
+/// Output: Success - в случае, если все скобки верны, т. е. на каждую открывающую, есть закрывающая. Если нет, индекс первой закрывающей скобки, для которой нет открывающей
+/// Если нет такой скобки, тогда первый индекс открывающей скобки для которой нет открывающей
+public class Task1 {
+    
+    public init() {
+    }
+    
+    /*
+     Sample Input 1:
+     
+     ([](){([])})
+     Sample Output 1:
+     
+     Success
+     Sample Input 2:
+     
+     ()[]}
+     Sample Output 2:
+     
+     5
+     Sample Input 3:
+     
+     {{[()]]
+     Sample Output 3:
+     
+     7
+     */
+    
+    /// - Parameter brackets: ( [ { ) ] }
+    public func isCorrectBrackets(brackets: String) {
+        func isClosedBracket(character: Character) -> Bool {
+            return [")", "}", "]"].contains(character)
+        }
+        
+        func isOppositeBracket(_ bracket: Character, for openBracket: Character) -> Bool {
+            return openBracket == "(" && bracket == ")" ||
+                openBracket == "[" && bracket == "]" ||
+                openBracket == "{" && bracket == "}"
+        }
+        
+        func isBracket(character: Character) -> Bool {
+            let brackets: [Character] = ["(", "[", "{", ")", "]", "}"]
+            return brackets.contains(character)
+        }
+        
+        let stack = Stack<(Character, Int)>()
+        var hasClosedBracketInString: Bool = false
+        for (index, character) in brackets.enumerated() {
+            if !isBracket(character: character) {
+                continue
+            }
+            
+            if isClosedBracket(character: character) {
+                hasClosedBracketInString = true
+                if let top = stack.top(), isOppositeBracket(character, for: top.0) {
+                    stack.pop()
+                } else {
+                    // перва
+                    print("\(index+1)")
+                    return
+                }
+            } else {
+                stack.add((character, index+1))
+            }
+        }
+        
+        if stack.isEmpty() {
+            if hasClosedBracketInString {
+                print("Success")
+            } else {
+                print("0")
+            }
+            
+            return
+        }
+        
+        if hasClosedBracketInString, let top = stack.top() {
+            print(top.1)
+            
+            return
+        }
+        
+        for (index, element) in brackets.enumerated() {
+            if !hasClosedBracketInString {
+                if isBracket(character: element) {
+                    print(index+1)
+                    return
+                }
+            }
+        }
+        
+        print("nothing")
+    }
+}
