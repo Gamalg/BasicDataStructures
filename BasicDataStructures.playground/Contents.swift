eb1e0af8d0a80bc8a2e7c95158b7fa2373d96421
Kubeyev Gamal <kubeyev.g@kolesa.kz>
2020-05-09 15:40:51 +0600

    Linked List

--- BasicDataStructures.playground/Contents.swift
+++ BasicDataStructures.playground/Contents.swift
@@ -1,2 +1,44 @@
 import UIKit
 
+//let node1 = Node(value: "1")
+//let node2 = Node(value: "2")
+//let node3 = Node(value: "3")
+//
+//node1.next = node2
+//node2.next = node3
+//node2.previous = node1
+//node3.previous = node2
+
+//let linkedList = LinkedList(head: node1)
+//
+//print(linkedList)
+
+func test() -> String {
+    let string = "zzzwwzzyz"
+    var map = [Character: Int]()
+    var indexArray = [Character]()
+    for char in string {
+        if let value = map[char] {
+            map[char] = value + 1
+        } else {
+            map[char] = 1
+        }
+        
+        if !indexArray.contains(char) {
+            indexArray.append(char)
+        }
+    }
+    
+    var resultString = ""
+    for char in indexArray {
+        if let mappedCharValue = map[char] {
+            resultString += "\(mappedCharValue)\(char)"
+        }
+    }
+    
+    print(resultString)
+    
+    return resultString
+}
+
+test()
--- BasicDataStructures.playground/Sources/LinkedList.swift
+++ BasicDataStructures.playground/Sources/LinkedList.swift
@@ -1,15 +1,50 @@
 import Foundation
 
-class Node<T> {
-    var next: Node?
-    var value: T
+public class Node<T> {
+    public var next: Node<T>?
+    public var value: T
+    public var previous: Node<T>?
     
-    init(value: T) {
+    public init(value: T) {
         self.value = value
     }
 }
 
-class LinkedList<T> {
-    var head: Node<T>
-    var tail: Node<T>
+public class LinkedList<T> {
+    public var head: Node<T>
+    public var tail: Node<T>
+    
+    public var description: String {
+        return "Head: \(head.value), Tail: \(tail.value)"
+    }
+    
+    public init(head: Node<T>) {
+        self.head = head
+        var node: Node<T>?
+        while head.next != nil {
+            node = head.next
+        }
+        
+        if node == nil {
+            node = head
+        }
+        
+        self.tail = node!
+    }
+    
+    public func append(node: Node<T>) {
+        
+    }
+    
+    public func insert(_ nodeToInsert: Node<T>, after node: Node<T>) {
+        
+    }
+    
+    public func removeNodeFromBegin() {
+        
+    }
+    
+    public func removeNodeFromEnd() {
+        
+    }
 }
